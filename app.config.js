5371ba529e68545e72ff7fe63c8cb53f79a4cc2d
Albert-CP <albertcp.acp@gmail.com>
2025-11-14 11:35:49 -0400

    changes .env and version update

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
     "name": "RendiPlay",
     "description": "Aplicación para la gestión de eventos deportivos",
     "slug": "sport-app",
-    "version": "1.0.7",
+    "version": "1.0.8",
     "owner": "integgreteam",
     "orientation": "portrait",
     "icon": "./assets/images/logo.png",
